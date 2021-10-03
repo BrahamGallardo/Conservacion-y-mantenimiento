493710fd7d6c7bac73912e6ed9a3dcf4060a8303
BrahamGallardo <braham.gc@gmail.com>
2021-10-03 16:28:30 -0500

    Commit de inicio

---
+++ sistema_departamento/src/main/java/com/access/dep/SistemaDepartamentoApplication.java
@@ -0,0 +1,13 @@
+package com.access.dep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDepartamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDepartamentoApplication.class, args);
+	}
+
+}
