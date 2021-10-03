50cc87f55bcc457b0ade320c5eb413285d103259
BrahamGallardo <braham.gc@gmail.com>
2021-10-03 16:28:30 -0500

    Commit de inicio, close #1

---
+++ sistema_departamento/src/test/java/com/access/dep/SistemaDepartamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.access.dep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDepartamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
